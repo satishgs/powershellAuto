c619b116003550d36b8556ecd2b3f343459084cb
subrahmanyagonella <satishgs@outlook.com>
2023-06-16 12:59:28 +0100

    Added script for copy from network drive

--- PrintWindowTitles.java
+++ PrintWindowTitles.java
@@ -6,11 +6,14 @@ import java.util.List;
 public class PrintWindowTitles {
     public static void main(String[] args) {
         try {
-            // Get a list of all open windows
-            List<Window> windows = Desktop.getDesktop().getWindows();
+            // Describe a generic JavaWindow object
+            JavaWindow genericWindow = new JavaWindow();
+
+            // Get a list of all top-level Java windows
+            List<JavaWindow> windows = genericWindow.getTopLevelWindows();
 
             // Loop through the list and print out the title of each window
-            for (Window window : windows) {
+            for (JavaWindow window : windows) {
                 System.out.println("Window title: " + window.getWindowTitle());
             }
 
