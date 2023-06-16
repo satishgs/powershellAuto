6e5db02e1d183da2d1c6a634a8b807be099fe858
subrahmanyagonella <satishgs@outlook.com>
2023-06-16 19:16:55 +0100

    Added script for copy from network drive

--- installMaven.ps1
+++ installMaven.ps1
@@ -1,32 +1,37 @@
-# Define the network path and installation directory
-$networkPath = "\\server\share\maven"
-$installDir = "C:\path\to\maven"
+# Define your paths
+$networkPath = "<network_path>\apache-maven-3.8.2-bin.zip"
+$localPath = "<local_path>"
 
-# Copy Maven files from network path to local installation directory
-try {
-    Write-Host "Copying Maven files..."
-    Copy-Item -Path $networkPath -Destination $installDir -Recurse -ErrorAction Stop
-    Write-Host "Maven files copied successfully!"
-}
-catch {
-    Write-Host "Failed to copy Maven files from the network path. Please check the network connectivity and paths."
-    exit 1
+# Check if local path exists, create it if it does not
+if (!(Test-Path -Path $localPath)) {
+    Write-Host "Creating destination directory..."
+    New-Item -ItemType Directory -Force -Path $localPath
 }
 
-# Set environment variables
-$env:MAVEN_HOME = $installDir
-$env:Path += ";$env:MAVEN_HOME\bin"
+# Define path to the copied zip file and the path where it should be extracted
+$zipFile = Join-Path -Path $localPath -ChildPath "apache-maven-3.8.2-bin.zip"
+$extractPath = $localPath
 
-# Validate Maven installation
-$mvnCmdPath = Join-Path $env:MAVEN_HOME 'bin\mvn.cmd'
-if (Test-Path $mvnCmdPath) {
-    Write-Host "Maven installation successful!"
+# Check if Maven zip file already exists locally, copy it from network if it does not
+if (!(Test-Path -Path $zipFile)) {
+    Write-Host "Copying Maven package from network path..."
+    Copy-Item -Path $networkPath -Destination $localPath
+}
 
-    # Print Maven version
-    Write-Host "Maven version:"
-    & $mvnCmdPath -version
+# Extract the Maven archive if it has not been extracted
+$mavenHome = Join-Path -Path $localPath -ChildPath "apache-maven-3.8.2"
+if (!(Test-Path -Path $mavenHome)) {
+    Write-Host "Extracting Maven package..."
+    Expand-Archive -Path $zipFile -DestinationPath $extractPath
 }
-else {
-    Write-Host "Maven installation failed. Please check the installation directory."
-    exit 1
+
+# Set up environment variables if they are not already set
+if (![Environment]::GetEnvironmentVariable("M2_HOME", "User") -or ![Environment]::GetEnvironmentVariable("PATH", "User").Contains("$mavenHome\bin")) {
+    Write-Host "Setting up environment variables..."
+    [Environment]::SetEnvironmentVariable("M2_HOME", $mavenHome, "User")
+    [Environment]::SetEnvironmentVariable("PATH", "$env:PATH;$env:M2_HOME\bin", "User")
 }
+
+# Validate the installation
+Write-Host "Validating the installation..."
+mvn -version
